0f98fdf52fdcd70558bd18c9a3b98850ad4f4aa1
Joey Schoblaska <joey.schoblaska@gmail.com>
2018-12-27 11:23:50 -0600

    added template and rendering

--- lib/writeup.rb
+++ lib/writeup.rb
@@ -1,8 +1,24 @@
 require "pygments.rb"
 require "html/pipeline"
 require "task_list/filter"
+require "erb"
 
 module Writeup
+  def self.render(markup, destination)
+    raise "destination must end in .html" unless destination =~ /\.html$/
+
+    @document = Writeup::Document.new(markup)
+    result = template.result(binding)
+    destination_dir = File.dirname(destination)
+    stylesheet_path = "#{File.dirname(__FILE__)}/../stylesheets/compiled/writeup.css"
+
+    File.open(destination, "w") { |f| f.puts(result) }
+    `cp "#{stylesheet_path}" "#{destination_dir}/writeup.css"`
+  end
+
+  def self.template
+    ERB.new(File.read("#{File.dirname(__FILE__)}/../layout/template.html.erb"))
+  end
 end
 
 Dir["#{File.dirname(__FILE__)}/writeup/**/*.rb"].each { |f| require f }
